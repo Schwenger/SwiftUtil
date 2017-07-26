ec118af7d43f05945e3d2424f2941ce685057cbd
Maximilian Schwenger <schwenger@react.uni-saarland.de>
2017-07-26 16:35:35 +0200

    Revised yield.

--- Sources/SwiftUtil/Miscellaneous/Util.swift
+++ Sources/SwiftUtil/Miscellaneous/Util.swift
@@ -27,3 +27,18 @@ public func seq2dict<Key,Value>(tupleSeq: [(Key, Value)]) -> [Key:Value] {
     tupleSeq.forEach{ res[$0.0] = $0.1 }
     return res
 }
+
+public func yield<T>(_ generator: () throws -> T, until pred: (T) throws -> Bool) rethrows -> [T] {
+    var res = [T]()
+    var cont = true
+    while cont {
+        res.append(try generator())
+        cont = try pred(res.head!)
+    }
+    return res
+}
+
+public func yield<T: OptionalType>(_ generator: () throws -> T) rethrows -> [T.Wrapped] {
+    let res: [T] = try yield(generator, until: { $0.optional.empty })
+    return res.flatten()
+}
