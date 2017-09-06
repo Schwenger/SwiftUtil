baff9bc7bc87bed98250aeede64632a6e8c90866
Maximilian Schwenger <schwenger@react.uni-saarland.de>
2017-09-06 17:06:38 +0200

    Created suitable Package.swift.

--- Package.swift
+++ Package.swift
@@ -1,7 +1,21 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "SwiftUtil"
+    name: "SwiftUtil",
+    products: [
+        .library(
+            name: "SwiftUtil",
+            targets: ["SwiftUtil"]),
+    ],
+    targets: [
+        .target(
+            name: "SwiftUtil",
+            dependencies: []),
+        .testTarget(
+            name: "SwiftUtilTests",
+            dependencies: ["SwiftUtil"]),
+    ]
 )
