59d72ddd6de86187988dec510323f9cf993c45bd
Maximilian Schwenger <schwenger@react.uni-saarland.de>
2017-12-13 12:28:18 +0100

    Extensions for Sequences containing collections or optionals.

--- Sources/SwiftUtil/Extensions/Sequence.swift
+++ Sources/SwiftUtil/Extensions/Sequence.swift
@@ -5,13 +5,6 @@
 //  Created by Maximilian Schwenger on 24.07.17.
 //
 
-public extension Sequence where Iterator.Element: OptionalType {
-    func flatten() -> [Iterator.Element.Wrapped] {
-        return self.filter{ $0.optional != nil }.map{ $0.optional! }
-    }
-}
-
-
 public extension Sequence {
     /*
      Split the given sequence into a satisfying (`t`) and a non-satisfying (`f`) part and
@@ -27,4 +20,27 @@ public extension Sequence {
         })
     }
 
+  public func zipWith<T>(_ map: (Iterator.Element) -> T) -> [(Iterator.Element, T)] {
+    return self.map{ ($0, map($0))}
+  }
+}
+
+public extension Sequence where Iterator.Element: OptionalType {
+  /*
+   Array containing the content of all non-empty elements. `nil` are removed.
+   Example: [Some(1), Some(4), nil, Some(3)].flattened == [1,4,3]
+  */
+  public var flattened: [Iterator.Element.Wrapped] {
+    return self.filter{ $0.optional != nil }.map{ $0.optional! }
+  }
+}
+
+public extension Sequence where Iterator.Element: Collection {
+  /*
+   Array containing the order-preserving union of all elements.
+   Example: [[2,3], [1,9]].joined == [2,3,1,9]
+  */
+  public var joined: [Iterator.Element.Element] {
+    return self.reduce([],+)
+  }
 }
